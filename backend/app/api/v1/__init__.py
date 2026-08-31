f0a396f0dd768b17e38742728b5f667780f25ba4
Lakshdeep Singh <lakshdeepofficial@gmail.com>
2026-08-31 11:32:02 +0530

    feat(data): add image proxy and WebP conversion

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1 import profile, recommend, reviews, search, room, movie
+from app.api.v1 import profile, recommend, reviews, search, room, movie, images
 
 api_router = APIRouter()
 api_router.include_router(recommend.router)
@@ -8,3 +8,4 @@ api_router.include_router(room.router)
 api_router.include_router(profile.router)
 api_router.include_router(reviews.router)
 api_router.include_router(movie.router)
+api_router.include_router(images.router)
